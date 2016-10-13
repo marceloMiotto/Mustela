3996619757e42df1f4bee85c2144e5b15a82ef3d
Marcelo_Miotto <mmiotto@gmail.com>
2016-10-13 18:56:18 -0300

    Initial Version

--- app/src/main/java/udacitynano/com/br/mustela/fragments/MainActivityFragment.java
+++ app/src/main/java/udacitynano/com/br/mustela/fragments/MainActivityFragment.java
@@ -3,14 +3,19 @@ package udacitynano.com.br.mustela.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import udacitynano.com.br.mustela.R;
 
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements MyDialogFragment.UserNameListener,View.OnClickListener {
 
+
+    Button mDialogFragment;
 
     public MainActivityFragment() {
         // Required empty public constructor
@@ -21,7 +26,34 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_activity, container, false);
+        View view =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        mDialogFragment = (Button) view.findViewById(R.id.showCustomFragment);
+        mDialogFragment.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onFinishUserDialog(String user) {
+        Toast.makeText(getActivity(), "Hello, " + user, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick(View view) {
+        // close existing dialog fragments
+        FragmentManager manager = getFragmentManager();
+        Fragment frag = manager.findFragmentByTag("fragment_edit_name");
+        if (frag != null) {
+            manager.beginTransaction().remove(frag).commit();
+        }
+        switch (view.getId()) {
+            case R.id.showCustomFragment:
+                MyDialogFragment editNameDialog = new MyDialogFragment();
+                editNameDialog.show(manager, "fragment_edit_name");
+                break;
+
+        }
+    }
 }
+
+
---
+++ app/src/main/java/udacitynano/com/br/mustela/fragments/MyDialogFragment.java
@@ -0,0 +1,54 @@
+package udacitynano.com.br.mustela.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import udacitynano.com.br.mustela.R;
+
+public class MyDialogFragment extends DialogFragment implements TextView.OnEditorActionListener {
+
+    private EditText mEditText;
+
+
+    public interface UserNameListener {
+        void onFinishUserDialog(String user);
+    }
+
+    // Empty constructor required for DialogFragment
+    public MyDialogFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_name, container);
+        mEditText = (EditText) view.findViewById(R.id.username);
+
+        // set this instance as callback for editor action
+        mEditText.setOnEditorActionListener(this);
+        mEditText.requestFocus();
+       /*
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+                */
+        getDialog().setTitle("Please enter username");
+
+        return view;
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        // Return input text to activity
+        UserNameListener activity = (UserNameListener) getActivity();
+        activity.onFinishUserDialog(mEditText.getText().toString());
+        this.dismiss();
+        return true;
+    }
+}
